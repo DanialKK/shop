1b5cc88d5aa8eda4429aedd0420b761d72c7c3a7
Danial <danial.khoshkhan@gmail.com>
2025-08-08 16:00:43 +0330

    Added accounts app and modified settings/urls

--- shophd/urls.py
+++ shophd/urls.py
@@ -7,9 +7,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+
     # API Routes
     path('api/', include(shop_api_urls)),
     path('api/comments/', include(comment_api_urls)),
+    path('api/accounts/', include('accounts.urls')),
 
     # JWT Auth
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
