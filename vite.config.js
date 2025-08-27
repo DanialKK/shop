5f2f0ed33f100889e190f3b2bfb905bca06f53d4
homow <homowsh897@gmail.com>
2025-08-27 15:17:32 +0330

    clean configs and README

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,6 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, "index.html"),
                 shop: resolve(__dirname, "shop/index.html"),
-                admin: resolve(__dirname, "admin/index.html"),
                 account: resolve(__dirname, "account/index.html"),
             }
         }
