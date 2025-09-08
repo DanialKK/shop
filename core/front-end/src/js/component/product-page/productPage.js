cdac2fe367c6cf985fc17d1f2a0189765536da04
homow <homowsh897@gmail.com>
2025-09-08 21:11:44 +0330

    update product id in productPage

--- core/front-end/src/js/component/product-page/productPage.js
+++ core/front-end/src/js/component/product-page/productPage.js
@@ -24,7 +24,7 @@ const productPage = (data) => {
     const tags = data?.tags_detail.map(tag => `<a href="" class="sub-text">#${tag.name.replace(" ", "_")}</a>`);
 
     return `
-<div data-id="1" class="bg-card-bg rounded-xl border-b-custom-border pb-6 overflow-hidden mx-auto w-11/12 xs:max-w-130 md:max-w-320 md:flex md:flex-row-reverse md:justify-between md:pt-6">
+<div data-id=${data.id} class="bg-card-bg rounded-xl border-b-custom-border pb-6 overflow-hidden mx-auto w-11/12 xs:max-w-130 md:max-w-320 md:flex md:flex-row-reverse md:justify-between md:pt-6">
     <!-- product gallery --> 
     <div class="basis-5/12 md:pl-4">
         <!-- base image -->
@@ -131,7 +131,7 @@ const productPage = (data) => {
             
             <!-- prodcut add to cart -->
             <div class="max-w-max mt-4 xs:mt-0">
-                <a href="/product/?id=1" class="primary-btn flex items-center gap-0.5">
+                <button data-product-id=${data.id} type="button" class="primary-btn flex items-center gap-0.5">
                     <span data-prodcut-text-add-to-cart="" class="text-sm font-normal">
                         افزودن به سبد خرید
                     </span>
@@ -139,7 +139,7 @@ const productPage = (data) => {
                     <svg data-product-icon-add-to-cart="false" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"></path>
                     </svg>
-                </a>
+                </button>
             </div>
         </div>
         
