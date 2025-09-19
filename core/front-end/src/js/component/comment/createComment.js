e00a6a2ed9f17ae744f3f437ceae39d07e94e198
homow <homowsh897@gmail.com>
2025-09-19 21:24:41 +0330

    completed UI create comment Component

--- core/front-end/src/js/component/comment/createComment.js
+++ core/front-end/src/js/component/comment/createComment.js
@@ -1,6 +1,7 @@
 const template = () => {
-    return `<form onsubmit="(function(e) {e.preventDefault()})(e);">
-    <textarea class="" name="" id="" cols="30" rows="10"></textarea>
+    return `<form class="flex flex-col gap-5 shadow-2xl p-4 rounded-lg mb-20" onsubmit="(function(e) {e.preventDefault()})(e);">
+    <p>نظر خودت رو بنویس</p>
+    <textarea placeholder="نوشتن نظر" class="border-2 border-primary-button-bg rounded-lg p-4" name="" id="" cols="30" rows="10"></textarea>
     <button class="primary-btn rounded-sm" type="submit">ثبت نظر</button>
 </form>`;
 }
