2f3f3183d30a67762c35848cbabbdddb224b7be7
homow <homowsh897@gmail.com>
2025-09-19 20:53:40 +0330

    add template of create comment Componenet

---
+++ core/front-end/src/js/component/comment/createComment.js
@@ -0,0 +1,8 @@
+const template = () => {
+    return `<form onsubmit="event => event.preventDefault()">
+    <textarea class="" name="" id="" cols="30" rows="10"></textarea>
+    <button class="primary-btn rounded-sm" type="submit">ثبت نظر</button>
+</form>`;
+}
+
+export default template;
